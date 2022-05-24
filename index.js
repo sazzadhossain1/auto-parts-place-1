27d8ab70333ba5f85dcfee2843987dfb9cec3a0e
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-24 18:45:54 +0600

    currection product by id

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
   } finally {
   }
 }
