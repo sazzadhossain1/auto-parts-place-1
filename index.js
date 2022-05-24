660efe2f1436e0d6e37cf50614c634d69b2672f7
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-24 17:56:33 +0600

    delete some code for currection

--- index.js
+++ index.js
@@ -20,22 +20,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
-    const serviceCollection = client.db("autoParts").collection("product");
-
-    app.get("/product", async (req, res) => {
-      const query = {};
-      const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
-    });
-
-    app.get("/product/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const service = await serviceCollection.findOne(query);
-      res.send(service);
-    });
   } finally {
   }
 }
